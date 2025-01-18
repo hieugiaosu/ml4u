32fa3c3230ef239cccc2c3d3ae776e0948fcf60e
hieugiaosu <phamthehieuvtt123@gmail.com>
2025-01-18 15:29:08 +0700

    update relative path

--- backend/src/modules/auth/dto/auth.dto.ts
+++ backend/src/modules/auth/dto/auth.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { UserDto } from "src/modules/user/dto/user.dto";
+import { UserDto } from "../../user/dto/user.dto";
+
 
 export class AuthInfoDto {
     @ApiProperty({
